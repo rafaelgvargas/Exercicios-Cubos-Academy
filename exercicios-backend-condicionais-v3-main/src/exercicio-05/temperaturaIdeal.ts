ff0eb5f9e66b855a6cb9a4a037be4ef62b726da8
Rafael <Rafaelgvargas@live.com>
2024-09-07 00:34:31 -0300

    Exercicio-05 Resolvido

--- exercicios-backend-condicionais-v3-main/src/exercicio-05/temperaturaIdeal.ts
+++ exercicios-backend-condicionais-v3-main/src/exercicio-05/temperaturaIdeal.ts
@@ -1,3 +1,12 @@
 export default function solucao(cerveja: string, temperatura: number): string {
-  // seu código aqui
+
+  if (cerveja === "pilsen" && temperatura >= 0 && temperatura <= 4) {
+    return 'TEMPERATURA IDEAL'
+} else if (cerveja === "trigo" && temperatura >= 4 && temperatura <= 6) {
+    return 'TEMPERATURA IDEAL'
+} else if (cerveja === "ipa" && temperatura >= 7 && temperatura <= 10) {
+    return 'TEMPERATURA IDEAL'
+} else {
+    return 'TEMPERATURA NAO IDEAL'
+}
 }
