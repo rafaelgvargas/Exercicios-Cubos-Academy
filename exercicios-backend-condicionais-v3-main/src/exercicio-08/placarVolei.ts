25a81115e7f5a6e46a3765136f1464753263f71b
Rafael <Rafaelgvargas@live.com>
2024-09-08 00:25:12 -0300

    Exercicio-08 Resolvido

--- exercicios-backend-condicionais-v3-main/src/exercicio-08/placarVolei.ts
+++ exercicios-backend-condicionais-v3-main/src/exercicio-08/placarVolei.ts
@@ -1,3 +1,14 @@
 export default function solucao(pontosA: number, pontosB: number): string {
-  // seu código aqui
+
+  if (pontosA >= 25 && pontosA > (pontosB + 1)) {
+    return 'TIME A VENCEU O SET'
+  } else if (pontosB >= 25 && pontosB > (pontosA+ 1)) {
+    return 'TIME B VENCEU O SET'
+  } else if (pontosA > pontosB) {
+    return 'TIME A VENCENDO'    
+  } else if (pontosB > pontosA) {
+    return 'TIME B VENCENDO'
+  } else {
+    return 'JOGO EMPATADO'
+  }
 }
