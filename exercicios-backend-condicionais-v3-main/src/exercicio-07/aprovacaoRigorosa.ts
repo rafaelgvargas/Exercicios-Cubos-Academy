5a691355343431e314fb1e93b7b0e23a56618e31
Rafael <Rafaelgvargas@live.com>
2024-09-07 23:41:21 -0300

    Exercicio-07 Resolvido

--- exercicios-backend-condicionais-v3-main/src/exercicio-07/aprovacaoRigorosa.ts
+++ exercicios-backend-condicionais-v3-main/src/exercicio-07/aprovacaoRigorosa.ts
@@ -3,5 +3,15 @@ export default function solucao(
   frequencia: number,
   projetoFinal: number
 ): string {
-  // seu código aqui
+
+  if (media >= 60 && frequencia >= 75 && projetoFinal > 0) {
+    return 'APROVADO'
+} 
+  if (media < 60 && frequencia >= 75 && projetoFinal >= 85) {
+    return 'APROVADO'
+  }
+  else {
+    return 'REPROVADO'
+}
+
 }
