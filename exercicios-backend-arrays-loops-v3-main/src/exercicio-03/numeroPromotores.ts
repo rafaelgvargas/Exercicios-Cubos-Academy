7d0bd0a441c612e81a44115541d17e0fec804c5f
Rafael <Rafaelgvargas@live.com>
2024-09-12 15:12:15 -0300

    Exercicio-03 Resolvido

--- exercicios-backend-arrays-loops-v3-main/src/exercicio-03/numeroPromotores.ts
+++ exercicios-backend-arrays-loops-v3-main/src/exercicio-03/numeroPromotores.ts
@@ -1,5 +1,12 @@
-function solucao(notas: number[]): number {
-  // seu código aqui
+function solucao(notas: number[]): number {    
+    let PROMOTORES = 0
+
+    for (let i =0; i <notas.length; i++) {
+        if (notas[i] >= 9){
+            PROMOTORES++
+        }
+    }
+        return PROMOTORES
 }
 
 export default solucao;
