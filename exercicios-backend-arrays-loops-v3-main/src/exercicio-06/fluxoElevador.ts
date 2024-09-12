c23f74ac4e89d8a4c13070278340ce84c8f9a211
Rafael <Rafaelgvargas@live.com>
2024-09-12 18:32:50 -0300

    Exercicio-06 Resolvido

--- exercicios-backend-arrays-loops-v3-main/src/exercicio-06/fluxoElevador.ts
+++ exercicios-backend-arrays-loops-v3-main/src/exercicio-06/fluxoElevador.ts
@@ -1,5 +1,20 @@
 function solucao(botoesApertados: string[]): string {
-  // seu código aqui
+  let botoesA1 = 0
+  let botoesA2 = 0
+  for (let i = 0; i < botoesApertados.length; i++) {
+      if (botoesApertados[i]=== 'A1') {
+        botoesA1++
+      } else if(botoesApertados[i] === 'A2'){
+        botoesA2++
+      }  
+    }  
+    if (botoesA1 > botoesA2){
+    return 'NERD'
+  } else if (botoesA2 > botoesA1) {
+    return 'ATLETA'
+  } else {
+    return 'DIVERSIFICADO'
+  }
 }
 
 export default solucao;
