492a770718a4bf1098880267847a1d5a43ace443
Rafael <Rafaelgvargas@live.com>
2024-09-12 16:31:40 -0300

    Exercicio-05 Resolvido

--- exercicios-backend-arrays-loops-v3-main/src/exercicio-05/sinucaBrasileira.ts
+++ exercicios-backend-arrays-loops-v3-main/src/exercicio-05/sinucaBrasileira.ts
@@ -1,5 +1,18 @@
 function solucao(jogadorA: number[]): string {
-  // seu código aqui
+  const Total = 120
+  let somajogadorA = 0
+    for (let i = 0; i < jogadorA.length; i++) {
+      somajogadorA += jogadorA[i]
+  }
+  const somajogadorB = Total - somajogadorA
+  if (somajogadorA > somajogadorB) {
+    return 'JOGADOR A GANHOU'
+  } else if (somajogadorB > somajogadorA) {
+    return 'JOGADOR B GANHOU'
+  } else {
+    return 'EMPATE'
+  }{
+    
+  }
 }
-
 export default solucao;
