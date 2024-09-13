b75044abc5fd9368559884df28a52bdcd51a4b0d
Rafael <Rafaelgvargas@live.com>
2024-09-13 00:20:52 -0300

    Exercicio-07 Resolvido

--- exercicios-backend-arrays-loops-v3-main/src/exercicio-07/contadorVogais.ts
+++ exercicios-backend-arrays-loops-v3-main/src/exercicio-07/contadorVogais.ts
@@ -1,5 +1,13 @@
 function solucao(palavra: string): boolean {
-  // seu código aqui
-}
+  const vogais = ['a', 'e', 'i', 'o', 'u']
 
+    for (let vogal of vogais) {
+      for (let letra of palavra ) {
+        if (letra === vogal) {
+          return true
+        }
+      }
+    }
+    return false
+}
 export default solucao;
