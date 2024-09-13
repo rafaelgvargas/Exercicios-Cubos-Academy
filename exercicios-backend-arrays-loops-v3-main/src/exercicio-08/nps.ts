29dff2759ed78975808a8d69c8c715601ecd5c1e
Rafael <Rafaelgvargas@live.com>
2024-09-13 01:13:21 -0300

    Exercicio-08 Resolvido

--- exercicios-backend-arrays-loops-v3-main/src/exercicio-08/nps.ts
+++ exercicios-backend-arrays-loops-v3-main/src/exercicio-08/nps.ts
@@ -1,5 +1,19 @@
 function solucao(notas: number[]): number {
-  // seu código aqui
+  let promotores = 0
+  let detratores = 0
+  const total = notas.length
+
+  for (let i = 0; i < total; i++){
+    if (notas[i] >=9) {
+      promotores++
+    } else if (notas[i] <=6) {
+      detratores++
+    }
+  }
+  
+  const nps =((promotores - detratores)/ total) * 100
+
+  return nps
 }
 
 export default solucao;
