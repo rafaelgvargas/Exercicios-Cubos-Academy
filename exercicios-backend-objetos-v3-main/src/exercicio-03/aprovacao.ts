4154a0f49a0170d36ebc83aa5e57460ed3e67a09
Rafael <Rafaelgvargas@live.com>
2024-09-26 23:59:44 -0300

    Exercicio-03 Resolvido

--- exercicios-backend-objetos-v3-main/src/exercicio-03/aprovacao.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-03/aprovacao.ts
@@ -1,7 +1,11 @@
 type TAluno = { matricula: string; nome: string; media: number };
 
 function solucao(aluno: TAluno): string {
-  // seu código aqui
+  if (aluno.media >= 6) {
+    return 'APROVADO';
+  } else {
+    return 'REPROVADO';
+  }
 }
 
 export default solucao;
