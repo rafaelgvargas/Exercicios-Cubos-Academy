ddb2dacf212acb1e5023c0d2a2c2d23f2a4a6dcf
Rafael <Rafaelgvargas@live.com>
2024-09-26 23:54:17 -0300

    Exercicio-02 Resolvido

--- exercicios-backend-objetos-v3-main/src/exercicio-02/cardapio.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-02/cardapio.ts
@@ -1,7 +1,14 @@
 type TPrato = { nome: string; valor: number; quantidade: number };
 
 function solucao(informacoesPrato: TPrato): number | string {
-  // seu código aqui
+
+  if (informacoesPrato.quantidade > 0) {
+    return informacoesPrato.valor;
+  }  else {
+    return "PRATO INDISPONIVEL";
+  }
+  
 }
+
 
 export default solucao;
