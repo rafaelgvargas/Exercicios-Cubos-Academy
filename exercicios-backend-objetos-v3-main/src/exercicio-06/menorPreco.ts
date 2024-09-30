ced74087a05ad91684b7cc6cbd07da97e7b1d794
Rafael <Rafaelgvargas@live.com>
2024-09-30 20:57:55 -0300

    Exercicio-06 Resolvido

--- exercicios-backend-objetos-v3-main/src/exercicio-06/menorPreco.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-06/menorPreco.ts
@@ -1,7 +1,16 @@
 type TProduto = { loja: string; preco: number };
 
 function solucao(produtosEncontrados: TProduto[]): string {
-  // seu código aqui
+  const precos = {
+    loja1: 40.99,
+    loja2: 20,
+    loja3: 30,
+  }
+  const menorPreco = produtosEncontrados.reduce((a, b) => {
+    return a.preco < b.preco ? a : b
+  })
+
+  return menorPreco.loja
 }
 
 export default solucao;
