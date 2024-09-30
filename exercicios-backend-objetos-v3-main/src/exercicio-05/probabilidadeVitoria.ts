e33751dd4f73441e8bad9e3449757a17f0e4f8ed
Rafael <Rafaelgvargas@live.com>
2024-09-30 20:51:53 -0300

    Exercicio-05 Resolvido

--- exercicios-backend-objetos-v3-main/src/exercicio-05/probabilidadeVitoria.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-05/probabilidadeVitoria.ts
@@ -1,7 +1,14 @@
 type TDados = { nome: string; probabilidades: number[] };
 
 function solucao(dados: TDados): number | string {
-  // seu código aqui
-}
+  const { probabilidades } = dados
 
+  if (probabilidades.length === 0) {
+    return 'DADOS INSUFICIENTES'
+  }
+  const soma = probabilidades.reduce((a, b) => a + b, 0)
+  const media = soma / probabilidades.length
+
+  return media
+}
 export default solucao;
