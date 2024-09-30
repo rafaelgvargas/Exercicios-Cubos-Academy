9240a53518385ff91235487ac479ee4daa0af5ba
Rafael <Rafaelgvargas@live.com>
2024-09-30 20:17:34 -0300

    Exercicio-04 Resolvido

--- exercicios-backend-objetos-v3-main/src/exercicio-04/melhorFilme.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-04/melhorFilme.ts
@@ -1,7 +1,16 @@
 type TFilme = { nome: string; notas: number[] };
 
 function solucao(filme1: TFilme, filme2: TFilme): string {
-  // seu código aqui
+  
+  const media1 = (filme1.notas[0] + filme1.notas[1]) / 2;
+  const media2 = (filme2.notas[0] + filme2.notas[1]) / 2;
+  
+  if (media1 > media2) {
+    return filme1.nome
+  } else if (media2 > media1) {
+    return filme2.nome
+  } else {
+    return 'EMPATE'
+  }
 }
-
 export default solucao;
