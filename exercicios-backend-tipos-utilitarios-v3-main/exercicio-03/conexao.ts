e347f40f77bb3e56d186e7982dc6ba4930109bce
Rafael <Rafaelgvargas@live.com>
2024-10-22 22:55:11 -0300

    Exercicio-02 Resolvido

--- exercicios-backend-tipos-utilitarios-v3-main/exercicio-03/conexao.ts
+++ exercicios-backend-tipos-utilitarios-v3-main/exercicio-03/conexao.ts
@@ -6,6 +6,11 @@ type Conn = {
     dbname: string
 }
 
+type ReadonlyConn = {
+    readonly driver: string,
+    readonly url: string
+}
+
 const conexao = (dados: Conn) => {
     const { username, password, host, port, dbname } = dados;
     return {
