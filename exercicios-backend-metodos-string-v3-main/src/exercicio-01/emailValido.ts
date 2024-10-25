2d59caac8683dc78fa62b4167492a950850bd848
Rafael <Rafaelgvargas@live.com>
2024-10-25 20:26:46 -0300

    Exercicio-01 Resolvido

--- exercicios-backend-metodos-string-v3-main/src/exercicio-01/emailValido.ts
+++ exercicios-backend-metodos-string-v3-main/src/exercicio-01/emailValido.ts
@@ -1,4 +1,8 @@
 function solucao(email: string): string {
-  // seu código aqui
+  if (email.includes("@")) {
+    return 'VALIDO'
+  } else {
+    return 'INVALIDO'
+  }
 }
 export default solucao;
