4c043fff6069f228f5814feb8364350beee42df9
Rafael <Rafaelgvargas@live.com>
2024-10-25 21:37:19 -0300

    Exercicio-03 Resolvido

--- exercicios-backend-metodos-string-v3-main/src/exercicio-03/recuperarSenha.ts
+++ exercicios-backend-metodos-string-v3-main/src/exercicio-03/recuperarSenha.ts
@@ -1,5 +1,5 @@
 function solucao(numeroCadastrado: string): string {
-  // seu código aqui
+  return numeroCadastrado.slice(0, 2) + "*".repeat(numeroCadastrado.length - 4) + numeroCadastrado.slice(-2)
 }
 
 export default solucao;
