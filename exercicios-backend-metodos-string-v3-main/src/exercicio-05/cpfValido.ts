756508de4f3e13185cb00342c374bfddc555e843
Rafael <Rafaelgvargas@live.com>
2024-10-25 22:37:13 -0300

    Exercicio-05 Resolvido

--- exercicios-backend-metodos-string-v3-main/src/exercicio-05/cpfValido.ts
+++ exercicios-backend-metodos-string-v3-main/src/exercicio-05/cpfValido.ts
@@ -1,5 +1,14 @@
 function solucao(cpf: string): string {
-  // seu código aqui
+  
+  const cpfValido = cpf.replace('-', '')
+  if (cpfValido.length !== 11) {
+    return 'CPF INVALIDO'
+  }
+    const porPartes = cpf.split('-')
+    if (porPartes.length !== 2 || porPartes[1].length !== 2) {
+      return 'CPF INVALIDO'
+    }
+  return 'CPF VALIDO'
 }
 
 export default solucao;
