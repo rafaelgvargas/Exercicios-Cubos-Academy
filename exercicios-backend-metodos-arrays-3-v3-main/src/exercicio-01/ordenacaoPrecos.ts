b6471d183faa1847d2224c08cc7821c29463520d
Rafael <Rafaelgvargas@live.com>
2024-10-31 17:40:57 -0300

    Exercicio-01 Resolvido

--- exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-01/ordenacaoPrecos.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-01/ordenacaoPrecos.ts
@@ -1,7 +1,11 @@
 type TFiltroPreco = "crescente" | "decrescente";
 
 function solucao(precos: number[], filtroPreco: TFiltroPreco): number[] {
-  // seu código aqui
+  if (filtroPreco === "crescente") {
+    return precos.sort((a, b) => a - b);
+  } else {
+    return precos.sort((a, b) => b - a);
+  }
   
 }
 export default solucao;
