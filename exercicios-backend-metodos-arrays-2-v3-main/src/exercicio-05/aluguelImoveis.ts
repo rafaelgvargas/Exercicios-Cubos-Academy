5d6e5bd65aa8f6e5a7124f13465feb8500ca407d
Rafael <Rafaelgvargas@live.com>
2024-10-31 14:10:37 -0300

    Exercicio-05 Resolvido

--- exercicios-backend-metodos-arrays-2-v3-main/src/exercicio-05/aluguelImoveis.ts
+++ exercicios-backend-metodos-arrays-2-v3-main/src/exercicio-05/aluguelImoveis.ts
@@ -1,10 +1,17 @@
-// IMPLEMENTE AQUI A SUA FUNÇÃO
 type TImovel = {
   titulo: string;
   descricao: string;
   preco: number;
 };
 function solucao(anuncios: TImovel[], precoMaximo: number): TImovel[] | string {
-  // seu código aqui
+  const filtro = anuncios.filter((anuncio) => {
+    return anuncio.preco <= precoMaximo
+  })
+
+  if (filtro.length > 0) {
+    return filtro
+  } else {
+    return 'NAO ENCONTRADO'
+  }
 }
 export default solucao;
