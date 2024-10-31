dcb8a552b7ff0e08b93a7076d6ae78b9eeee73ef
Rafael <Rafaelgvargas@live.com>
2024-10-31 18:38:28 -0300

    Exercicio-04 Resolvido

--- exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-04/maioresCampeoes.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-04/maioresCampeoes.ts
@@ -5,7 +5,16 @@ type TTime = {
 };
 
 function solucao(times: TTime[], paisFiltrado: string): TTime[] | string {
-  // seu código aqui
+  const timesFiltrados = times.filter((time) => {
+    return time.pais === paisFiltrado
+  })
+  const timesOrdenados = timesFiltrados.sort((a, b) => {
+    return b.quantidadeTitulos - a.quantidadeTitulos
+  })
+  if (timesFiltrados.length === 0) {
+    return 'NAO ENCONTRADO'
+  }
+  return timesOrdenados
   
 }
 export default solucao;
